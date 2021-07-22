5cd17068f6655f41b6534e8463e365d690874128
Nishtha511 <nishthahv@gmail.com>
2021-07-22 19:26:27 +0530

    Nishtha's Portfolio

---
+++ js/bootstrap/script.js
@@ -0,0 +1,14 @@
+/*preloader*/
+$(window).on('load',function(){
+    $('#status').fadeOut();
+    $('#preloader').delay(400).fadeOut();
+});
+http.createServer(
+    function (req, res) { ... }).listen(3013);
+    
+    "parserOptions": {
+  "ecmaVersion": 6,
+  "ecmaFeatures": {
+    "experimentalObjectRestSpread": true
+  }
+},
